46e4a2e7596f40bdd6f8c7d55fb4d4f0e0ee37a3
Nicholas Anderson <nichranderson@icloud.com>
2024-08-05 11:26:59 -0400

    Node v20 Upgrades

--- index.js
+++ index.js
@@ -8,43 +8,44 @@ const REGION = "us-east-1"; //e.g. "us-east-1"
 const ddbClient = new DynamoDBClient({ region: REGION });
 
 const { BatchWriteItemCommand } = require("@aws-sdk/client-dynamodb");
-console.log('Loading function');
+console.log("Loading function");
 
-exports.handler = async (event) => {
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
+exports.handler = async (event, context) => {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(
       process.env.AWS_EXECUTION_ENV
-      ? '/opt/nodejs/node_modules/@sparticuz/chromium/bin'
-      : undefined,
-      ),
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-      });
-    const page = await browser.newPage();
-  
-    await page.goto('https://lodgemaster-client.oa-bsa.org/');
-  
+        ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin"
+        : undefined
+    ),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+  const page = await browser.newPage();
+
+  try {
+    await page.goto("https://lodgemaster-client.oa-bsa.org/");
+
     // Set screen size
-    await page.setViewport({width: 1080, height: 1024});
-  
+    await page.setViewport({ width: 1080, height: 1024 });
+
     // Type into username box
-    await page.type('#Username', process.env.USERNAME);
+    await page.type("#Username", process.env.USERNAME);
 
     // Type into password box
-    await page.type('#Password', process.env.PASSWORD);
-  
+    await page.type("#Password", process.env.PASSWORD);
+
     // Wait and click on submit
-    const submitSelector = 'button.btn.btn-secondary.btn-lg';
+    const submitSelector = "button.btn.btn-secondary.btn-lg";
     await page.waitForSelector(submitSelector);
     await page.click(submitSelector);
 
-    await page.waitForNetworkIdle({idleTime: 5000});
+    await page.waitForNetworkIdle({ idleTime: 5000 });
 
     const cookiesSet = await page.cookies();
 
-    const p = JSON.parse(JSON.stringify(cookiesSet))
+    const p = JSON.parse(JSON.stringify(cookiesSet));
 
     // Set the parameters
     const params = {
@@ -53,39 +54,38 @@ exports.handler = async (event) => {
           {
             PutRequest: {
               Item: {
-                name: { S: p[0]['name'] },
-                value: { S: p[0]['value'] },
+                name: { S: p[0]["name"] },
+                value: { S: p[0]["value"] },
               },
             },
           },
           {
             PutRequest: {
               Item: {
-                name: { S: p[1]['name'] },
-                value: { S: p[1]['value'] },
+                name: { S: p[1]["name"] },
+                value: { S: p[1]["value"] },
               },
             },
           },
           {
             PutRequest: {
               Item: {
-                name: { S: p[2]['name'] },
-                value: { S: p[2]['value'] },
+                name: { S: p[2]["name"] },
+                value: { S: p[2]["value"] },
               },
             },
           },
           {
             PutRequest: {
               Item: {
-                name: { S: p[3]['name'] },
-                value: { S: p[3]['value'] },
+                name: { S: p[3]["name"] },
+                value: { S: p[3]["value"] },
               },
             },
           },
         ],
       },
     };
-
     const run = async () => {
       try {
         const data = await ddbClient.send(new BatchWriteItemCommand(params));
@@ -96,7 +96,8 @@ exports.handler = async (event) => {
       }
     };
     run();
+  } finally {
+    await page.close();
     await browser.close();
-
-  };
-
+  }
+};
